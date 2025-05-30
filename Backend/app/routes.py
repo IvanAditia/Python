e3bc83f40dd34c9e5580c2a7852c1596e3c1daba
Ivan Antyo <ivan@CANDI.LAN>
2025-05-30 17:43:55 +0700

    lupa update apa

--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, Request
+from flask import Blueprint, Request, jsonify
 
 auth = Blueprint('auth', __name__)
 
-@auth.route("/", methods=['GET', 'POST'])
-def main():
-  return 
+@auth.route("/input-konsumen", methods=['GET', 'POST'])
+def input_konsumen():
+  data = request.get_json()
+  name = data.get('name')
+  jenis = data.get('jenis')
+  alamat = data.get('alamat')
+  wa = data.get('wa')
